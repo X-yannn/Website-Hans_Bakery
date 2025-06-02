f8ee1b8e87e2baed63b1bc8c7dbd4529e17b117b
X-yannn <septianmaulanabarkah@gmail.com>
2025-06-02 12:12:35 +0700

    Add files via upload

---
+++ TOKO-ONLINE-ROTI/about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">HANS_BAKERY adalah sebuah toko roti rumahan yang berada di Kota Tasikmalaya</p>
+<p>
+HANS_BAKERYy adalah salah satu UMKM dari bidang industri roti yang berada di wilayah kota Tasikmalaya, tepatnya di Cipawitra, Mangkubumi, Kota.Tasikmalaya.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
